7049b531bee1af1c5e92e366329898d2ac237cf1
Chris DuBose <chris.dubose@swgas.com>
2016-11-29 13:44:27 -0800

    not all successful objects returned have a value field

--- src/main/java/com/swgas/parser/ObjectParser.java
+++ src/main/java/com/swgas/parser/ObjectParser.java
@@ -15,18 +15,32 @@ public class ObjectParser<T> implements MarionetteParser<T>{
     public T parseFrom(String s) {
         JsonObject success = get(s);
         JsonValue v;
-        if(success.getValueType() == JsonValue.ValueType.NULL || null == (v = success.get("value"))){
+        if(success.getValueType() == JsonValue.ValueType.NULL){
             return null;
         }
-        switch(v.getValueType()){
-            case NULL:   return null;
-            case NUMBER: return (T)Double.valueOf(((JsonNumber)v).doubleValue());
-            case STRING: return (T)((JsonString)v).getString();
-            case OBJECT: return (T)v;
-            case ARRAY:  return (T)((JsonArray)v).stream().map(Objects::toString).collect(Collectors.<String>toList());
-            case FALSE:  return (T)Boolean.FALSE;
-            case TRUE:   return (T)Boolean.TRUE;
-            default:     return null;
+        v = success.get("value");
+        if(null != v){
+            switch(v.getValueType()){
+                case NULL:   return null;
+                case NUMBER: return (T)Double.valueOf(((JsonNumber)v).doubleValue());
+                case STRING: return (T)((JsonString)v).getString();
+                case OBJECT: return (T)v;
+                case ARRAY:  return (T)((JsonArray)v).stream().map(Objects::toString).collect(Collectors.<String>toList());
+                case FALSE:  return (T)Boolean.FALSE;
+                case TRUE:   return (T)Boolean.TRUE;
+                default:     return null;
+            }
+        } else {
+            switch(success.getValueType()){
+                case NULL:   return null;
+                case NUMBER: return (T)Double.valueOf(((JsonNumber)success).doubleValue());
+                case STRING: return (T)((JsonString)success).getString();
+                case OBJECT: return (T)success;
+                case ARRAY:  return (T)((JsonArray)success).stream().map(Objects::toString).collect(Collectors.<String>toList());
+                case FALSE:  return (T)Boolean.FALSE;
+                case TRUE:   return (T)Boolean.TRUE;
+                default:     return null;
+            }
         }
     }
 }
