0f46618dff6d53d4423b8ccf9f6db9c5c805f13d
Chris DuBose <chris.dubose@swgas.com>
2016-12-01 13:35:58 -0800

    trying to fix jsonstring lists

--- src/main/java/com/swgas/parser/ArrayParser.java
+++ src/main/java/com/swgas/parser/ArrayParser.java
@@ -2,6 +2,7 @@ package com.swgas.parser;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.json.JsonArray;
 import javax.json.JsonNumber;
@@ -10,13 +11,16 @@ import javax.json.JsonString;
 import javax.json.JsonValue;
 
 public class ArrayParser implements MarionetteParser<List<String>> {
+    private static final Logger LOG = Logger.getLogger(ArrayParser.class.getName());
+    
     @Override
     public List<String> parseFrom(String s) {
         JsonArray success = getArray(s);
         if (success.getValueType() == JsonValue.ValueType.NULL) {
             return null;
-        }
+        }        
         return success.stream().map(j -> {
+            LOG.info(String.format("type: %s", j.getValueType()));
             switch(j.getValueType()){
                 case STRING: return ((JsonString)j).getString();
                 case NULL:   return null;
