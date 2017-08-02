49576cab308430198978541fb1b88a29cbcc86b9
Chris DuBose <chris.dubose@swgas.com>
2017-08-02 08:01:06 -0700

    element

---
+++ src/main/java/com/swgas/model/WebElement.java
@@ -0,0 +1,52 @@
+package com.swgas.model;
+
+import com.swgas.rest.Jsonable;
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class WebElement implements Jsonable<WebElement> {
+    public static final String ID  = "element-6066-11e4-a52e-4f735466cecf";
+    public static final String KEY = "ELEMENT";
+    
+    private String id;
+    
+    public WebElement(){}
+    
+    public WebElement(String id){
+        this.id = id;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toJson(){
+        return toString();
+    }
+    
+    @Override
+    public WebElement fromJson(String json){
+        JsonObject _json = Json.createReader(new StringReader(json)).readObject();
+        this.id = _json.getString(KEY);
+        return this;
+    }
+    
+    @Override
+    public String toString(){
+        return Json.createObjectBuilder()
+        .add("element", id)
+        .build().toString();
+    }
+}
