579bdbd89b378e851f543bfbb705d0686708ac1a
Chris DuBose <chris.dubose@swgas.com>
2017-08-31 14:34:34 -0700

    logging

--- src/main/java/com/swgas/marionette/MarionetteFactory.java
+++ src/main/java/com/swgas/marionette/MarionetteFactory.java
@@ -55,7 +55,7 @@ public class MarionetteFactory {
             Files.newBufferedWriter(profileDirectory.resolve("user.js")).append("user_pref(\"marionette.defaultPrefs.port\", 0);").append(System.lineSeparator()).close();
             ProcessBuilder procBuilder = new ProcessBuilder("firefox", "-marionette", "-profile", profileDirectory.toString(), "-new-instance");
             Process proc = procBuilder.start();
-            LOG.info(proc.info().commandLine().orElse(""));
+            LOG.info(proc.info().commandLine().orElse(""+proc.info()));
             int port = CompletableFuture.supplyAsync(() -> new BufferedReader(new InputStreamReader(proc.getInputStream())).lines()
                 .mapToInt(
                     line -> {
