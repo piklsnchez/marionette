d77e686abaf52c151144be2d51a9e159075ad31c
Chris DuBose <chris.dubose@swgas.com>
2017-09-06 16:21:17 -0700

    read output from proc

--- src/main/java/com/swgas/marionette/MarionetteFactory.java
+++ src/main/java/com/swgas/marionette/MarionetteFactory.java
@@ -50,8 +50,10 @@ public class MarionetteFactory {
     
     public static CompletableFuture<Session> createSession(){
         CompletableFuture<Session> ret = new CompletableFuture<>();
+        Session session = new Session();
         try{
             Path profileDirectory = Files.createTempDirectory("marionette");
+            session.setProfileDirectory(profileDirectory);
             Files.newBufferedWriter(profileDirectory.resolve("user.js"))
             .append("user_pref(\"marionette.defaultPrefs.port\", 0);").append(System.lineSeparator())
             .append("user_pref(\"browser.startup.homepage_override.mstone\", \"ignore\");").append(System.lineSeparator()).close();
@@ -59,47 +61,66 @@ public class MarionetteFactory {
             ProcessBuilder procBuilder = new ProcessBuilder(commandList);
             procBuilder.command(commandList);
             Process proc = procBuilder.start();
+            session.setProc(proc);
             LOG.info(proc.info().toString());
-            int port;
-            try{
-                port = CompletableFuture.supplyAsync(() -> new BufferedReader(new InputStreamReader(proc.getInputStream())).lines()
-                    .mapToInt(
-                        line -> {
-                            //LOG.info(line);
-                            Matcher match = PATTERN.matcher(line);
-                            if(match.find()){
-                                String _port = match.group(1);
-                                LOG.info(_port);
-                                if(_port.codePoints().allMatch(Character::isDigit)){
-                                    return Integer.parseInt(_port, 10);
-                                }
-                            }
-                            return 0;
-                        }
-                    ).filter(p -> p > 0)
-                    .findFirst().orElseThrow(UnknownErrorException::new)
-                ).get(10, TimeUnit.SECONDS);
-            } catch(TimeoutException e){
-                proc.destroy();
-                new Session(null, proc, null, profileDirectory).close();
-                throw e;
-            }
+            int port = getPort(proc);
             AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
             channel.connect(new InetSocketAddress("localhost", port), ret, new CompletionHandler<Void, CompletableFuture>(){
                 @Override
                 public void completed(Void result, CompletableFuture future) {
-                    Session session = new Session(null, proc, new MarionetteImpl(channel), profileDirectory);
+                    session.setClient(new MarionetteImpl(channel));
                     proc.onExit().thenRun(() -> session.close());
                     future.complete(session);
                 }
                 @Override
                 public void failed(Throwable e, CompletableFuture future) {
                     future.completeExceptionally(new MarionetteException((e.getCause() instanceof ConnectException) ? e.getCause() : e));
-                }            
+                }
             });
         } catch(IOException | InterruptedException | ExecutionException | TimeoutException e){
+            if(e instanceof TimeoutException){            
+                session.getProc().destroy();
+            }
             throw new MarionetteException(e);
         }
         return ret;
+    }
+    
+    private static int getPort(Process proc) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        String output = "";
+        byte[] buff = new byte[255];
+        while(0 < proc.getInputStream().read(buff)){
+            output += new String(buff);            
+            Matcher match = PATTERN.matcher(output);
+            if(match.find()){
+                String _port = match.group(1);
+                LOG.info(String.format("port: %d", _port));
+                if(_port.codePoints().allMatch(Character::isDigit)){
+                    return Integer.parseInt(_port, 10);
+                }
+            }
+        }
+        LOG.info(output);
+        return 0;
+    }
+    
+    private static int getPortFancy(Process proc) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+        return CompletableFuture.supplyAsync(() -> new BufferedReader(new InputStreamReader(proc.getInputStream())).lines()
+            .mapToInt(
+                line -> {
+                    //LOG.info(line);
+                    Matcher match = PATTERN.matcher(line);
+                    if(match.find()){
+                        String _port = match.group(1);
+                        LOG.info(_port);
+                        if(_port.codePoints().allMatch(Character::isDigit)){
+                            return Integer.parseInt(_port, 10);
+                        }
+                    }
+                    return 0;
+                }
+            ).filter(p -> p > 0)
+            .findFirst().orElseThrow(UnknownErrorException::new)
+        ).get(10, TimeUnit.SECONDS);
     }
 }
