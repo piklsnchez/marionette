0c013a0662675f415a1d500b98e8ee07ba1cd0fb
Christopher DuBose <CSD3@swgas.com>
2017-09-13 08:09:19 -0700

    win path bs

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-mvn clean package assembly:single -DskipTests=true && \
-$JAVA_HOME/bin/java -jar ~/.m2/repository/org/junit/platform/junit-platform-console-standalone/1.0.0-RC3/junit-platform-console-standalone-1.0.0-RC3.jar -classpath target/test-classes:target/marionette-2.0-SNAPSHOT-jar-with-dependencies.jar -m com.swgas.marionette.MarionetteImplTest#testFindElements
+CLASSPATH=target/test-classes:target/marionette-2.0-SNAPSHOT-jar-with-dependencies.jar 
+if [[ ! "$(uname)" =~ ^Linux.* ]]
+then
+  sep=";"
+  CLASSPATH=${CLASSPATH//:/$sep}
+  echo "CLASSPATH: $CLASSPATH"
+fi
+mvn clean package assembly:single -U -DskipTests=true && \
+"$JAVA_HOME/bin/java" -jar ~/.m2/repository/org/junit/platform/junit-platform-console-standalone/1.0.0-RC3/junit-platform-console-standalone-1.0.0-RC3.jar -classpath "$CLASSPATH" -m com.swgas.marionette.MarionetteImplTest#testFindElements
