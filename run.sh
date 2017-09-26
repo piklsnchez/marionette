d57a3fc7523a31578513c759c25c4f55189a589c
Chris DuBose <chris.dubose@swgas.com>
2017-09-26 10:29:02 -0700

    pom update

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ then
   echo "CLASSPATH: $CLASSPATH"
 fi
 mvn clean package assembly:single -U -DskipTests=true && \
-"$JAVA_HOME/bin/java" -jar ~/.m2/repository/org/junit/platform/junit-platform-console-standalone/1.0.0-RC3/junit-platform-console-standalone-1.0.0-RC3.jar -classpath "$CLASSPATH" -m com.swgas.marionette.MarionetteImplTest#testFindElements
+"$JAVA_HOME/bin/java" -jar ~/.m2/repository/org/junit/platform/junit-platform-console-standalone/1.0.0/junit-platform-console-standalone-1.0.0.jar -classpath "$CLASSPATH" -m com.swgas.marionette.MarionetteImplTest#testFindElements
